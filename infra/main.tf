6104941b61a0768e5ccecdb2e14d7654c8d565e3
jefferson <jeffersoonaraujoo@gmail.com>
2025-10-07 00:05:44 -0300

    Align private_subnet_ids variable for consistency in RDS module configuration

--- infra/main.tf
+++ infra/main.tf
@@ -3,8 +3,7 @@ module "rds_instance" {
   rds_identifier_name  = var.rds_identifier_name
   rds_sg_ids           = [data.terraform_remote_state.network.outputs.security_group_postgres_id]
   db_subnet_group_name = data.terraform_remote_state.network.outputs.private_subnet_group_name
-  private_subnet_ids   = data.terraform_remote_state.network.outputs.private_subnet_ids
-
+  private_subnet_ids    = data.terraform_remote_state.network.outputs.private_subnet_ids
   rds_username_secret_name = var.rds_username_ssm_path
   rds_password_secret_name = var.rds_password_ssm_path
 
