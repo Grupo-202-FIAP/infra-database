b2e6d8c13cce20c53efdd046069b0935c191e694
jefferson <jeffersoonaraujoo@gmail.com>
2025-10-06 23:19:46 -0300

    Update README and outputs for RDS module: enhance documentation and streamline outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,39 +1,14 @@
-output "vpc_id" {
-  description = "ID da VPC criada"
-  value       = module.vpc.vpc_id
+output "rds_endpoint" {
+  description = "Endpoint do banco de dados RDS"
+  value       = module.rds_instance.rds_endpoint
 }
 
-output "public_subnet_ids" {
-  description = "Lista de IDs das subnets públicas"
-  value       = module.public_subnet.public_subnet_ids
+output "rds_username" {
+  description = "Usuário administrador do RDS"
+  value       = module.rds_instance.rds_username
 }
 
-output "private_subnet_ids" {
-  description = "Lista de IDs das subnets privadas"
-  value       = module.private_subnet.private_subnet_ids
-}
-
-output "internet_gateway_id" {
-  description = "ID do Internet Gateway"
-  value       = module.internet_gateway.igw_id
-}
-
-output "route_table_id" {
-  description = "ID da Route Table"
-  value       = module.route_table.route_table_id
-}
-
-output "security_group_api_id" {
-  description = "ID do Security Group da API"
-  value       = module.security_group_api.security_group_id
-}
-
-output "security_group_postgres_id" {
-  description = "ID do Security Group do PostgreSQL"
-  value       = module.security_group_postgres.postgres_sg_id
-}
-
-output "network_acl_id" {
-  description = "ID do Network ACL"
-  value       = module.acl.acl_id
+output "rds_subnet_group" {
+  description = "Nome do subnet group criado"
+  value       = module.rds_instance.rds_subnet_group
 }
