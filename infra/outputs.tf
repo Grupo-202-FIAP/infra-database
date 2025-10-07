2b6f0aa58327dafef17265da89cd9fafb52615e4
jefferson <jeffersoonaraujoo@gmail.com>
2025-10-06 23:59:51 -0300

    Remove unused output variables from RDS module configuration

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,13 +2,3 @@ output "rds_endpoint" {
   description = "Endpoint do banco de dados RDS"
   value       = module.rds_instance.rds_endpoint
 }
-
-output "rds_username" {
-  description = "Usuário administrador do RDS"
-  value       = module.rds_instance.rds_username
-}
-
-output "rds_subnet_group" {
-  description = "Nome do subnet group criado"
-  value       = module.rds_instance.rds_subnet_group
-}
