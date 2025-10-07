64f34d61534ac89a3608896945ed0d3ba53f672f
Jefferson Araujo Silva <111441175+Jefferson-Araujo-Silva@users.noreply.github.com>
2025-10-06 23:13:04 -0300

    Implement RDS module with subnet group, instance configuration, and SSM parameters; add outputs for RDS details and network resources. (#9)

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,21 +1,85 @@
-variable "rds_identifier_name" {
-  description = "Identificador único da instância RDS (o nome da instância na AWS)."
+variable "rds_username" {
+  description = "Usuário administrador do RDS."
   type        = string
+  default     = "postgres"
 }
 
-variable "rds_sg_id" {
-  description = "ID do Security Group para o RDS."
+variable "rds_password" {
+  description = "Senha do administrador do RDS."
   type        = string
+  default     = ""
+}
+variable "vpc_name" {
+  type        = string
+  description = "Nome da VPC"
+  default     = ""
 }
 
-variable "db_subnet_group_name" {
-  description = "Nome do grupo de subnets privadas para o RDS."
+variable "subnet_name" {
   type        = string
+  description = "Prefixo para nome das subnets"
 }
 
-variable "private_subnet_ids" {
-  description = "Lista de IDs das subnets privadas."
+variable "nat_name" {
+  type        = string
+  description = "Nome do Nat Gateway"
+}
+
+variable "subnet_group_name" {
+  type        = string
+  description = "Nome para o grupo das subnets"
+}
+
+variable "igw_name" {
+  type        = string
+  description = "Nome do Internet Gateway"
+}
+
+variable "sg_api_name" {
+  type        = string
+  description = "Nome do Security Group da API"
+}
+
+variable "sg_postgres_name" {
+  type        = string
+  description = "Nome do Security Group do PostgreSQL"
+}
+
+variable "acl_name" {
+  type        = string
+  description = "Nome do Network ACL"
+}
+
+variable "cidr_block" {
+  type        = string
+  description = "CIDR da VPC"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags aplicadas aos recursos"
+}
+
+variable "azs" {
   type        = list(string)
+  description = "Zona de disponibilidade"
+}
+
+variable "public_subnets" {
+  type        = list(string)
+  description = "CIDR das subnets públicas"
+}
+
+variable "private_subnets" {
+  type        = list(string)
+  description = "CIDR da subnet privada"
+}
+
+variable "route_cidr" {
+  type        = string
+  default     = "0.0.0.0/0"
+  description = "CIDR para rota padrão"
 }
 
 variable "rds_username_ssm_path" {
@@ -48,8 +112,42 @@ variable "engine_version" {
   type        = string
 }
 
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "Tags aplicadas aos recursos"
+variable "api_gw_name" {
+  description = "Nome da API REST no API Gateway."
+  type        = string
+}
+
+variable "api_gw_description" {
+  description = "Descrição da API REST no API Gateway."
+  type        = string
+}
+
+variable "api_gw_root_path" {
+  description = "Nome do recurso raiz (root resource) para a API (ex: items, products)."
+  type        = string
+}
+
+variable "api_stage_name" {
+  description = "Nome do stage de deploy da API (ex: dev, hom, prod)."
+  type        = string
+}
+
+variable "rds_identifier_name" {
+  description = "Identificador único da instância RDS (o nome da instância na AWS)."
+  type        = string
+}
+
+variable "route_table_name" {
+  description = "Nome do recurso para a Tabela de Rotas Pública (ex: infra-public-rt)."
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Nome do bucket"
+  type        = string
+}
+
+variable "region" {
+  description = "Região da AWS"
+  type = string
 }
