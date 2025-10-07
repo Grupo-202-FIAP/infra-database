1ee133f0a5a2dd5858683c2436c96beb804e06ad
jefferson <jeffersoonaraujoo@gmail.com>
2025-10-06 23:58:30 -0300

    Remove unused variables from RDS module configuration

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,82 +1,3 @@
-variable "rds_username" {
-  description = "Usuário administrador do RDS."
-  type        = string
-  default     = "postgres"
-}
-
-variable "rds_password" {
-  description = "Senha do administrador do RDS."
-  type        = string
-  default     = ""
-}
-variable "vpc_name" {
-  type        = string
-  description = "Nome da VPC"
-  default     = ""
-}
-
-variable "subnet_name" {
-  type        = string
-  description = "Prefixo para nome das subnets"
-}
-
-variable "nat_name" {
-  type        = string
-  description = "Nome do Nat Gateway"
-}
-
-variable "subnet_group_name" {
-  type        = string
-  description = "Nome para o grupo das subnets"
-}
-
-variable "igw_name" {
-  type        = string
-  description = "Nome do Internet Gateway"
-}
-
-variable "sg_api_name" {
-  type        = string
-  description = "Nome do Security Group da API"
-}
-
-variable "sg_postgres_name" {
-  type        = string
-  description = "Nome do Security Group do PostgreSQL"
-}
-
-variable "cidr_block" {
-  type        = string
-  description = "CIDR da VPC"
-}
-
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "Tags aplicadas aos recursos"
-}
-
-variable "azs" {
-  type        = list(string)
-  description = "Zona de disponibilidade"
-}
-
-variable "public_subnets" {
-  type        = list(string)
-  description = "CIDR das subnets públicas"
-}
-
-variable "private_subnets" {
-  type        = list(string)
-  description = "CIDR da subnet privada"
-}
-
-variable "route_cidr" {
-  type        = string
-  default     = "0.0.0.0/0"
-  description = "CIDR para rota padrão"
-}
-
 variable "rds_username_ssm_path" {
   description = "Caminho (name) no SSM Parameter Store para o nome de usuário do RDS."
   type        = string
@@ -132,17 +53,7 @@ variable "rds_identifier_name" {
   type        = string
 }
 
-variable "route_table_name" {
-  description = "Nome do recurso para a Tabela de Rotas Pública (ex: infra-public-rt)."
-  type        = string
-}
-
 variable "bucket_name" {
   description = "Nome do bucket"
-  type        = string
-}
-
-variable "region" {
-  description = "Região da AWS"
   type        = string
 }
