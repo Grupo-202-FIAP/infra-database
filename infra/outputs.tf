c64f3051ab302c712475dc29b080e9314237b845
Matheus Fernandes Rodrigues <111136519+Fernandeess@users.noreply.github.com>
2026-01-07 02:11:22 -0300

    Update S3 backend configuration and outputs for RDS module (#11)
    Co-authored-by: fernandeeess <matheusfernandesrodrigues@outlook.com>

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,13 @@ output "rds_endpoint" {
   description = "Endpoint do banco de dados RDS"
   value       = module.rds_instance.rds_endpoint
 }
+
+output "rds_username" {
+  description = "Usuário do RDS"
+  value       = module.rds_instance.rds_username
+}
+
+output "rds_password_ssm_param" {
+  description = "Caminho do SSM Parameter Store com a senha do RDS"
+  value       = var.rds_password_ssm_path
+}
