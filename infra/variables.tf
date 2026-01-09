004d2dc9360f067b5afd23d31b53d305091814d7
Matheus Fernandes Rodrigues <111136519+Fernandeess@users.noreply.github.com>
2026-01-09 16:03:00 -0300

    Remove sg rule (#20)
    * Remove unused security group rule for EC2 access to RDS
    
    * Add EC2 Bastion module and related configurations
    
    * Fix EC2 Bastion module security group reference
    
    * Remove EC2 Bastion module and related configurations
    
    ---------
    
    Co-authored-by: fernandeeess <matheusfernandesrodrigues@outlook.com>

--- infra/variables.tf
+++ infra/variables.tf
@@ -61,34 +61,3 @@ variable "bucket_name" {
   description = "Nome do bucket"
   type        = string
 }
-
-# Bastion EC2 variables
-variable "bastion_instance_type" {
-  description = "Instance type for Bastion EC2 (ex: t3.micro, t3.small)"
-  type        = string
-}
-
-variable "bastion_instance_name" {
-  description = "Name for the Bastion EC2 instance"
-  type        = string
-}
-
-variable "bastion_ami" {
-  description = "AMI ID for Bastion (Amazon Linux 2 or Ubuntu 22.04)"
-  type        = string
-}
-
-variable "bastion_key_name" {
-  description = "Name for the Bastion Key Pair"
-  type        = string
-}
-
-variable "bastion_key_secret_name" {
-  description = "Name of the secret in AWS Secrets Manager for storing the private key"
-  type        = string
-}
-
-variable "bastion_root_volume_size" {
-  description = "Root volume size in GB for Bastion"
-  type        = number
-}
